3ab4480e790bc0a92bf4af0d441af4b5de2e4fb8
ffrat <ffrat@FPRAT17-DE>
2017-04-04 16:25:24 +0200

    Ajout tests unitaires

---
+++ Job Overview/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Job_Overview
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        // Tester si une exception est levée lorsqu'une tache annexe est créée avec un code déjà existant
+        public void TestAjouterTacheAnnexe()
+        {
+            Projet p = new Projet();
+            p.AjouterTache(new TacheAnnexe(10, "coucou10"));
+            // Ajout d'une tache annexe avec un code déjà existant
+            p.AjouterTache(new TacheAnnexe(10, "coucou20"));
+        }
+
+         [TestMethod]
+         // Création d'une tache annexe : vérifier que le cas où le code saisi n'est pas un entier, est bien géré
+         public void TestSaisirDonnéesTacheAnnexe()
+        {
+            int code;
+            string codeSaisi = "fsqfqsf";
+            Assert.AreEqual(false, int.TryParse(codeSaisi, out code));
+        }
+
+
+
+    
+    }
+}
